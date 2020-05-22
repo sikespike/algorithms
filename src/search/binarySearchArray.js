48b2b8637be330eab1264d9427ae621225faa4d0
Vladimir Kavoun <vkavoun@gmail.com>
2020-05-22 14:03:54 -0400

    fixing binary search

--- src/search/binarySearchArray.js
+++ src/search/binarySearchArray.js
@@ -3,28 +3,18 @@ import { quickSort } from '../sort/quickSort';
 const binarySearchMidPoint = (left, right) => Math.floor((right - left) / 2) + left;
 
 const recursiveBinarySearchArray = (input, item, left, right) => {
-  if (right < left) {
-    return -1;
+  const mid = binarySearchMidPoint(left, right);
+
+  if (input[mid] === item) {
+    return mid;
+  } else if (input[mid] < item) {
+    return recursiveBinarySearchArray(input, item, mid + 1, right);
   } else {
-    const mid = binarySearchMidPoint(left, right);
-
-    if (input[mid] > item) {
-      return recursiveBinarySearchArray(input, item, left, mid - 1);
-    } else if (input[mid] < item) {
-      return recursiveBinarySearchArray(input, item, mid + 1, right);
-    } else {
-      return mid;
-    }
+    return recursiveBinarySearchArray(input, item, left, mid - 1);
   }
 };
 
-const binarySearchArray = (input, item, sorted = false) => {
-  if (!sorted) {
-    input = quickSort(input);
-  }
-
-  return recursiveBinarySearchArray(input, item, 0, input.length - 1);
-};
+const binarySearchArray = (input, item) => recursiveBinarySearchArray(input, item, 0, input.length - 1);
 
 export default {
   binarySearchArray,
